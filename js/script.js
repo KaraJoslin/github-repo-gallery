a22894f2322596cc44e3b6fa0815c7d31ddacab6
Kara_J <kjoslin@avc.edu>
2023-05-20 08:24:32 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+//1st Section div in HTML index
+const overview = document.querySelector(".overview");
+const username = "KaraJoslin";
+
+const getUserInfo = async function () {
+    const userInfo = await fetch(`https://api.github.com/users/${username}`);
+    const data = await userInfo.json();
+    displayUserInfo(data);
+};
+
+getUserInfo();
+
+const displayUserInfo = function (data) {
+    const div = document.createElement("div");
+    div.classList.add("user-info");
+    div.innerHTML = `
+    <figure>
+      <img alt="user avatar" src=${data.avatar_url} />
+    </figure>
+    <div>
+      <p><strong>Name:</strong> ${data.name}</p>
+      <p><strong>Bio:</strong> ${data.name}</p>
+      <p><strong>Location:</strong> ${data.location}</p>
+      <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+    </div>
+    `;
+    overview.append(div);
+};
+
+
+
+
+
+
